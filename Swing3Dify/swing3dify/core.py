39bb7d779271c36538bb15d7b4e4a9720c23d319
kitano <7419040@ed.tus.ac.jp>
2023-09-24 23:21:03 +0900

    3次元復元に失敗

--- Swing3Dify/swing3dify/core.py
+++ Swing3Dify/swing3dify/core.py
@@ -2,7 +2,13 @@ import cv2
 import numpy as np
 
 
-def reconstruct_3D(pts1, pts2, K, R, T):
+def reconstruct_3D(
+    pts1: np.ndarray,
+    pts2: np.ndarray,
+    K: np.ndarray,
+    R: np.ndarray,
+    T: np.ndarray,
+):
     P1 = K.dot(np.hstack([np.eye(3), np.zeros((3, 1))]))
     P2 = K.dot(np.hstack([R, T.reshape(3, -1)]))
 
@@ -10,3 +16,17 @@ def reconstruct_3D(pts1, pts2, K, R, T):
 
     reconstructed_3D = homogeneous_3D[:3] / homogeneous_3D[3]
     return reconstructed_3D
+
+
+def compute_camera_parameters(
+    pts1: np.ndarray, pts2: np.ndarray, K: np.ndarray
+):
+    F, _ = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+    E = K.T.dot(F).dot(K)
+
+    U, S, Vt = np.linalg.svd(E)
+    W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    R = U.dot(W).dot(Vt)
+    T = U[:, 2]
+
+    return R, T
