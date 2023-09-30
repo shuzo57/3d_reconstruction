15948eaf389d79cc7bdb829cba29cc25d07b0cc2
kitano <7419040@ed.tus.ac.jp>
2023-10-01 01:00:56 +0900

    異常検知ライブラリの準備

--- Swing3Dify/swing3dify/__init__.py
+++ Swing3Dify/swing3dify/__init__.py
@@ -1,7 +1,7 @@
+from .config import *  # noqa
 from .core import *  # noqa
 from .time_adjustment import *  # noqa
 from .utils import *  # noqa
 from .visualizations import *  # noqa
-from .config import *  # noqa
-from .YoloPose import *  # noqa
 from .YoloClub import *  # noqa
+from .YoloPose import *  # noqa
