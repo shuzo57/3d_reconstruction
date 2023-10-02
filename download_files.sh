dc2536b05e8bceda4e727443353f949ac0c8fd20
kitano <kitano>
2023-10-02 13:08:01 +0900

    フォルダの作成の追加

--- download_files.sh
+++ download_files.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+DOWNLOAD_FOLDER="models"
+
+mkdir -p "${DOWNLOAD_FOLDER}"
+
 download_from_drive() {
     local FILE_ID="$1"
     local FILE_NAME="$2"
 
     curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
     local CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-    curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ${FILE_NAME}
+    curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o "${DOWNLOAD_FOLDER}/${FILE_NAME}"
 }
 
 download_from_drive "1G1YQkpQmPK9LXy6CJkTOjgeAs7dE9-mX" "club-s.pt"
